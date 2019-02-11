02c720f09b08e793d6c5ab5fae905ae4f25bf055
chao <zcsd2668@163.com>
2019-02-11 15:45:57 +0800

    实现生成jar文件与配置文件

--- src/test/com/pancake/util/RunUtil.java
+++ src/test/com/pancake/util/RunUtil.java
@@ -1,5 +1,7 @@
 package com.pancake.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pancake.entity.component.Transaction;
 import com.pancake.entity.content.TxString;
 import com.pancake.entity.enumeration.TxType;
@@ -11,8 +13,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by chao on 2019/2/3.
@@ -21,6 +26,8 @@ public class RunUtil {
     private final static Logger logger = LoggerFactory.getLogger(RunUtil.class);
     private TransactionService txService = TransactionService.getInstance();
     private BlockService blockService = BlockService.getInstance();
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     /**
      * 向队列中添加 tx
@@ -104,7 +111,7 @@ public class RunUtil {
         String txIdMsgCollection;
         String blockMsgCollection;
         List<NetAddress> blockerList = JsonUtil.getBlockerAddressList(Const.BlockChainConfigFile);
-        for(NetAddress blockerAddr : blockerList) {
+        for (NetAddress blockerAddr : blockerList) {
             mongoDB = new MongoDB(new NetAddress("127.0.0.1", 27017), Const.BLOCK_CHAIN);
             blockChainCollection = blockerAddr + "." + Const.BLOCK_CHAIN;
             lbiCollection = blockerAddr + "." + Const.LAST_BLOCK_ID;
@@ -137,6 +144,7 @@ public class RunUtil {
     @Test
     public void findUnCommitClientMsg() {
         String url;
+
         String commitMsgCountCollection;
         List<NetAddress> netAddresses = JsonUtil.getValidatorAddressList(Const.BlockChainConfigFile);
         MongoDB mongoDB;
@@ -153,5 +161,115 @@ public class RunUtil {
             }
 
         }
+    }
+
+    @Test
+    public void generateConfigFile() {
+
+        NetAddress validator1 = new NetAddress("127.0.0.1", 8000);
+        NetAddress validator2 = new NetAddress("127.0.0.1", 8001);
+        NetAddress validator3 = new NetAddress("127.0.0.1", 8002);
+        NetAddress validator4 = new NetAddress("127.0.0.1", 8003);
+
+        int validatorCount = 4;
+
+        List<NetAddress> validatorList = new ArrayList<NetAddress>();
+        validatorList.add(validator1);
+        validatorList.add(validator2);
+        validatorList.add(validator3);
+        validatorList.add(validator4);
+
+
+        Map<String, Object> configMap1 = new LinkedHashMap<String, Object>();
+        Map<String, Object> configMap2 = new LinkedHashMap<String, Object>();
+        Map<String, Object> configMap3 = new LinkedHashMap<String, Object>();
+        Map<String, Object> configMap4 = new LinkedHashMap<String, Object>();
+        // 1
+        configMap1.put("current_validator", validator1);
+        configMap2.put("current_validator", validator2);
+        configMap3.put("current_validator", validator3);
+        configMap4.put("current_validator", validator4);
+        // 2
+        configMap1.put("validators", validatorList);
+        configMap2.put("validators", validatorList);
+        configMap3.put("validators", validatorList);
+        configMap4.put("validators", validatorList);
+
+        Map<String, Object> mongoMap = new LinkedHashMap<String, Object>();
+        mongoMap.put("ip", "127.0.0.1");
+        mongoMap.put("port", 27017);
+        mongoMap.put("database", "BlockChain");
+        // 3
+        configMap1.put("mongodb", mongoMap);
+        configMap2.put("mongodb", mongoMap);
+        configMap3.put("mongodb", mongoMap);
+        configMap4.put("mongodb", mongoMap);
+
+        Map<String, Object> rabbitMap = new LinkedHashMap<String, Object>();
+        rabbitMap.put("ip", "127.0.0.1");
+        rabbitMap.put("port", 5672);
+        rabbitMap.put("userName", "admin");
+        rabbitMap.put("password", "admin");
+        // 3
+        configMap1.put("rabbitmq", rabbitMap);
+        configMap2.put("rabbitmq", rabbitMap);
+        configMap3.put("rabbitmq", rabbitMap);
+        configMap4.put("rabbitmq", rabbitMap);
+
+        Map<String, Object> keyPairMap = new LinkedHashMap<String, Object>();
+        keyPairMap.put("pvt_key_file", "./privateKey.txt");
+        keyPairMap.put("pub_key_file", "./publicKey.txt");
+        // 4
+        configMap1.put("key_pair", keyPairMap);
+        configMap2.put("key_pair", keyPairMap);
+        configMap3.put("key_pair", keyPairMap);
+        configMap4.put("key_pair", keyPairMap);
+
+        Map<String, Object> txTransmitterConfigMap = new LinkedHashMap<String, Object>();
+        txTransmitterConfigMap.put("limitTime", 500); //ms
+        txTransmitterConfigMap.put("limitSize", 4); //MB
+        // 5
+        configMap1.put("tx_transmitter", txTransmitterConfigMap);
+        configMap2.put("tx_transmitter", txTransmitterConfigMap);
+        configMap3.put("tx_transmitter", txTransmitterConfigMap);
+        configMap4.put("tx_transmitter", txTransmitterConfigMap);
+
+        String filePath1 = "node1.json";
+        String filePath2 = "node2.json";
+        String filePath3 = "node3.json";
+        String filePath4 = "node4.json";
+
+        String configData1 = "";
+        String configData2 = "";
+        String configData3 = "";
+        String configData4 = "";
+        try {
+            configData1 = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(configMap1);
+            configData2 = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(configMap2);
+            configData3 = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(configMap3);
+            configData4 = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(configMap4);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(new File(filePath1));
+            outputStream.write(configData1.getBytes(), 0, configData1.length());
+
+            outputStream = new FileOutputStream(new File(filePath2));
+            outputStream.write(configData2.getBytes(), 0, configData2.length());
+
+            outputStream = new FileOutputStream(new File(filePath3));
+            outputStream.write(configData3.getBytes(), 0, configData3.length());
+
+            outputStream = new FileOutputStream(new File(filePath4));
+            outputStream.write(configData4.getBytes(), 0, configData4.length());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
